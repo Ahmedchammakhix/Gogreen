e7765e6da4a6cf0e9812d2ad275cb21c64275094
Ahmedchammakhix <94017007+Ahmedchammakhix@users.noreply.github.com>
2021-12-08 01:14:32 +0100

    Add files via upload

---
+++ leftbar.php
@@ -0,0 +1,23 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Tableau de bord </a></li>
+			
+			<li><a href="userlist.php"><i class="fa fa-users"></i> Utilisateurs</a>
+			</li>
+			<li><a href="profile.php"><i class="fa fa-user"></i> &nbsp;Profile</a>
+			</li>
+			<li><a href="feedback.php"><i class="fa fa-envelope"></i> &nbsp;Messages</a>
+			</li>
+			<li><a href="notification.php"><i class="fa fa-bell"></i> &nbsp;Notification <sup style="color:red">*</sup></a>
+			</li>
+			<li><a href="deleteduser.php"><i class="fa fa-user-times"></i> &nbsp;Utilisaters supprimés</a>
+			</li>
+			<li><a href="download.php"><i class="fa fa-download"></i> &nbsp;Telecharger la liste d'utilisateurs</a>
+			</li>
+			</ul>
+			<p class="text-center" style="color:#ffffff; margin-top: 100px;">© </p>
+		</nav>
+
+		
